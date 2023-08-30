20b72cf678256e1ec7b1dff708e9cf1728feef26
patryk177m <patrykmajewski177@gmail.com>
2023-08-30 12:26:34 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const btn = document.getElementById('form');
+
+btn.addEventListener('submit', e => e.preventDefault());
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
